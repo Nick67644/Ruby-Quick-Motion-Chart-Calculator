f1ada61f4fd9e7ecd37245a12169a18b6c3545bb
Nick67644 <24.mathman890.mathman@gmail.com>
2022-12-27 01:12:43 -0600

    - Fixed tabs/spaces. - Added comments for explaining functions of the script.

--- index.rb
+++ index.rb
@@ -1,8 +1,11 @@
-puts "Quick Motion Chart calculator - Programmed in Ruby"
+# Start of script
 
+puts "Quick Motion Chart calculator - Programmed in Ruby"
 sleep 0.25
 
 puts "Enter a Velocity: "
+
+# Allows user to state/intake the amount of velocity
 
 def numberv()
   $numv = gets.chomp()
@@ -16,6 +19,8 @@ if validv
 end
 
 numberv()
+
+# Allows user to state/intake the amount of decreasing or increasing
 
 puts "Enter a rate of decrease or increase: "
 def numbera()
@@ -31,11 +36,13 @@ end
 
 numbera()
 
+# Allows you to get the users requested amount of answers
+
 nums = -1
 
 puts "How many outcomes do you want? "
 def numberout()
-$outcomes = gets.chomp()
+  $outcomes = gets.chomp()
 $outcomes_test = Integer($outcomes) rescue false
 if $outcomes_test
   else
@@ -47,22 +54,26 @@ end
 
 numberout()
 
+# Runs the Δx (DeltaX) formula
+
 until nums > $outcomes_test - 2
 
   if $outcomes == 1
     $outcomes = nums + 1
   end
-  nums = nums + 1
+    nums = nums + 1
 
-  num2 = $numv.to_i * nums.to_i
-  num3 = 0.5.to_f * $numa.to_i
-  num4 = num3.to_i * nums.pow(2).to_i
-  num5 = num2.to_i + num4.to_i
+    num2 = $numv.to_i * nums.to_i
+    num3 = 0.5.to_f * $numa.to_i
+    num4 = num3.to_i * nums.pow(2).to_i
+    num5 = num2.to_i + num4.to_i
 
   total = num5.to_i
 
-  puts("Total is " + total.to_s)
-  sleep 0.5
+    puts("Total is " + total.to_s)
+    sleep 0.5
 end
+
+# Finishes script
 
 puts("Script finished - Restart if you wish to run again.")
