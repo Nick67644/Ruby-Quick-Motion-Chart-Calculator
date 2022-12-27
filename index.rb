aa70b39e0366baf5f199c93cfd13d6a577945e8e
Nick67644 <24.mathman890.mathman@gmail.com>
2022-12-27 01:02:22 -0600

    - Added script finished. - Put all main .chomps into defs for easy editing and for better response/allows for responses if you mess up. - Removed ; as they are not useful in this code (Stupid Nick moment). - Changed basic capitalization.

--- index.rb
+++ index.rb
@@ -3,45 +3,66 @@ puts "Quick Motion Chart calculator - Programmed in Ruby"
 sleep 0.25
 
 puts "Enter a Velocity: "
-numv = gets.chomp()
-validv = Integer(numv) rescue false
+
+def numberv()
+  $numv = gets.chomp()
+validv = Integer($numv) rescue false
 if validv
-else
-  abort "Invalid - Enter valid number";
+  else
+    puts "Invalid - Enter valid number"
+    puts "Enter a Velocity: "
+    numberv()
+  end
 end
 
+numberv()
+
 puts "Enter a rate of decrease or increase: "
-numa = gets.chomp()
-valida = Integer(numa) rescue false
+def numbera()
+  $numa = gets.chomp()
+valida = Integer($numa) rescue false
 if valida
-else
-  abort "Invalid - Enter valid number";
+  else
+    puts "Invalid - Enter valid number"
+    puts "Enter a decrease or increase: "
+    numbera()
+  end
 end
+
+numbera()
 
 nums = -1
 
 puts "How many outcomes do you want? "
-outcomes = gets.chomp()
-outcomes_test = Integer(outcomes) rescue false
-if outcomes_test
-else
-  abort "Invalid - Enter valid number";
+def numberout()
+$outcomes = gets.chomp()
+$outcomes_test = Integer($outcomes) rescue false
+if $outcomes_test
+  else
+    puts "Invalid - Enter valid number"
+    puts "Enter a outcome: "
+    numberout()
+  end
 end
 
-until nums > outcomes_test - 2
+numberout()
 
-  if outcomes == 1
-    outcomes = nums + 1
+until nums > $outcomes_test - 2
+
+  if $outcomes == 1
+    $outcomes = nums + 1
   end
   nums = nums + 1
 
-  num2 = numv.to_i * nums.to_i
-  num3 = 0.5.to_f * numa.to_i
+  num2 = $numv.to_i * nums.to_i
+  num3 = 0.5.to_f * $numa.to_i
   num4 = num3.to_i * nums.pow(2).to_i
   num5 = num2.to_i + num4.to_i
 
   total = num5.to_i
 
-  puts("total is " + total.to_s)
+  puts("Total is " + total.to_s)
   sleep 0.5
 end
+
+puts("Script finished - Restart if you wish to run again.")
